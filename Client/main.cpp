c2852fb96628f9506bccc3fbbd64b551f204af24
ggggtxgt <3035886959@qq.com>
2026-01-24 18:48:00 +0800

    014随机驱动名称

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Windows.h>
+#include "resource.h"
 
 SC_HANDLE hManager = NULL;
 SC_HANDLE hService = NULL;
@@ -30,9 +31,32 @@ BOOL UninstallDriver() {
     return DeleteService(hService);
 }
 
+void RandomDriverName(char *driverName, DWORD len) {
+    char directory[20] = { 'A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'X'};
+    for (size_t i = 0; i < len; i++) {
+        srand(time(NULL));
+        int num = rand() % 20;
+        driverName[i] = directory[i];
+    }
+}
+
+void LoadRes() {
+    HRSRC hRsc = FindResourceA(NULL, MAKEINTRESOURCEA(IDR_MYSYS1), "MySys");
+    HGLOBAL hg = LoadResource(NULL, hRsc);
+    LPVOID fileBuff = LockResource(hg);
+    HANDLE hFile = CreateFileA("D:\\text.sys", FILE_ALL_ACCESS, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE, 
+                               NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    DWORD fileSize = SizeofResource(NULL, hRsc);
+    DWORD realWriteLen = 0;
+    WriteFile(hFile, fileBuff, fileSize, &realWriteLen, NULL);
+    CloseHandle(hRsc);
+}
+
 int main() {
-    // @todo 使用随机名称
-    InstallDriver("Driver", "Driver", "C:\\Users\\win10\Desktop\\Driver.sys");
+    LoadRes();
+    char strName[20] = { 0 };
+    RandomDriverName(strName, 19);
+    InstallDriver(strName, strName, "C:\\Users\\win10\Desktop\\Driver.sys");
     BOOL success = StartDriver();
     if (success) {
         printf("启动成功!");
