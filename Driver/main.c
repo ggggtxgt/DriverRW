eee6c4c30809c88ab4aef538dcbb3a83e46d7dae
ggggtxgt <3035886959@qq.com>
2026-01-24 23:41:31 +0800

    017注册回调实现进程保护

--- Driver/main.c
+++ Driver/main.c
@@ -1,18 +1,50 @@
 #include "util.h"
+#include "structure.h"
 #include "RwGetModule.h"
 #include "GetModuleUtil.h"
 
+HANDLE hCallback = NULL;
+
 void DriverUnload(PDRIVER_OBJECT pDriver) {
     // UnRegisterCallBack();
+    if (hCallback) {
+        ObUnRegisterCallbacks(hCallback);
+    }
+}
+
+OB_PREOP_CALLBACK_STATUS PobPreOperationCallback(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION OperationInformation) {
+    OperationInformation->Parameters->CreateHandleInformation.DesiredAccess = PROCESS_ALL_ACCESS;
+    OperationInformation->Parameters->CreateHandleInformation.OriginalDesiredAccess = PROCESS_ALL_ACCESS;
+    OperationInformation->Parameters->DuplicateHandleInformation.DesiredAccess = PROCESS_ALL_ACCESS;
+    OperationInformation->Parameters->CreateHandleInformation.OriginalDesiredAccess = PROCESS_ALL_ACCESS;
+    return OB_PREOP_SUCCESS;
+}
+
+void PobPostOperationCallback(PVOID RegistrationContext, POB_POST_OPERATION_INFORMATION OperationInformation) {
+
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING pRegPath) {
     pDriver->DriverUnload = DriverUnload;
+    PKLDR_DATA_TABLE_ENTRY pLdr = pDriver->DriverSection;
+    pLdr->Flags |= 0x20;  // 没有微软签名的作法
+    OB_OPERATION_REGISTRATION obOperRegister = { 0 };
+    obOperRegister.ObjectType = PsProcessType;
+    obOperRegister.Operations = OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE;
+    obOperRegister.PreOperation = PobPreOperationCallback;
+    obOperRegister.PostOperation = PobPostOperationCallback;
+    OB_CALLBACK_REGISTRATION obCallback = { 0 };
+    obCallback.Version = ObGetFilterVersion();
+    obCallback.OperationRegistrationCount = 1;
+    obCallback.RegistrationContext = NULL;
+    RtlInitUnicodeString(&obCallback.Altitude, L"obCallback.Altitude");
+    obCallback.OperationRegistration = &obOperRegister;
+    NTSTATUS status = ObRegisterCallbacks(&obCallback, &hCallback);
     /*
     RTL_OSVERSIONINFOW version = { 0 };
     version.dwOSVersionInfoSize = sizeof(RTL_OSVERSIONINFOW);
     RtlGetVersion(&version);    // 获取当前 Windows 版本，使用 version.dwBuildNumber 判断
     */
-    RegisterCallBack();
+    // RegisterCallBack();
     return STATUS_SUCCESS;
 }
--- Driver/structure.h
+++ Driver/structure.h
@@ -11,3 +11,24 @@ typedef struct _MY_MEMORY_BASIC_INFORMATION {
 	ULONG64 Protect;
 	ULONG64 Type;
 } MYMEMORY_BASIC_INFORMATION, *PMYMEMORY_BASIC_INFORMATION;
+
+typedef struct _KLDR_DATA_TABLE_ENTRY {
+    LIST_ENTRY InLoadOrderLinks;
+    PVOID ExceptionTable;
+    ULONG ExceptionTableSize;
+    // ULONG padding on IA64
+    PVOID GpValue;
+    ULONG64 NonPagedDebugInfo;
+    PVOID DllBase;
+    PVOID EntryPoint;
+    ULONG SizeOfImage;
+    UNICODE_STRING Ful1D11Name;
+    UNICODE_STRING BaseD11Name;
+    ULONG Flags;
+    USHORT LoadCount;
+    USHORT __Unused5;
+    PVOID SectionPointer;
+    ULONG CheckSum;
+    // ULONG padding on IA64PVOID LoadedImports;
+    PVOID PatchInformation;
+} KLDR_DATA_TABLE_ENTRY, *PKLDR_DATA_TABLE_ENTRY;
